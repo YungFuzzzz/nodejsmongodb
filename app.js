4608b178a1ba24628bce82258ea65baab05db2e7
FUZZZ <147380081+YungFuzzzz@users.noreply.github.com>
2024-10-11 15:11:15 +0200

    feat: added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 const config = require('config');
@@ -17,6 +18,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
